4854822ba105f5560cea38778ebe4665d483ef9f
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-05 18:35:02 +0300

    Form Validation 1

--- src/main/java/com/innova/dto/InnovaValidationDto.java
+++ src/main/java/com/innova/dto/InnovaValidationDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+import javax.validation.constraints.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,9 +15,21 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class InnovaValidationDto {
 
+    @NotEmpty(message = "Ad alanını boş geçemezsiniz")
     private String userName;
+
+    @NotEmpty(message = "Soyad alanını boş geçemezsiniz")
     private String userSurname;
+
+    @NotEmpty(message = "Email alanını boş geçemezsiniz")
+    @Email(message = "Uygun formatta mail giriniz")
     private String emailAddress;
+
+    @Min(value = 18, message = "18 yaşından küçüksünüz. Başvuramazsınız")
+    @Max(value = 45, message = "45 yaşından büyüksünüz. Başvuramazsınız")
     private int userAge;
+
+    @NotEmpty(message = "Mesaj alanını boş geçemezsiniz")
+    @Size(min = 1,max=255)
     private String userMessage;
 }
