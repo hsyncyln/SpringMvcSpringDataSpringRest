cb3750a53f3cf2dc61b9f8803642b1c2a5710616
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-05 19:17:11 +0300

    ComputerEntity.java

---
+++ src/main/java/com/innova/entity/ComputerEntity.java
@@ -0,0 +1,41 @@
+package com.innova.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "computer")
+public class ComputerEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private Long computerId;
+
+    @Column(name = "computer_name")
+    private String computerName;
+
+    @Column(name = "computer_trade")
+    private String computerTrade;
+
+    //@Column(name = "computer_price",columnDefinition = "Decimal(10,2) default='100.00'")
+    @Column(name = "computer_price")
+    private String computerPrice;
+
+    @Column(name="computer_serial_number",length = 11,nullable = false,unique = true)
+    private int serialComputerNumber;
+
+  //  @Lob //buyuk veriler icin
+  //  private  String bigData;
+
+  //  @Transient //Sadece Javada
+  //  private String justJava;
+
+    @Column(name = "computer_createdDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date createdDate;
+}
