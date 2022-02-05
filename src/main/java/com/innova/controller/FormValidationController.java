b59b273cb4304da08541bb20e79baee2bc45a2eb
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-05 18:22:46 +0300

    Form Validation 1

---
+++ src/main/java/com/innova/controller/FormValidationController.java
@@ -0,0 +1,28 @@
+package com.innova.controller;
+
+import com.innova.dto.InnovaValidationDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Log4j2
+public class FormValidationController {
+
+    // http://localhost:8090/formurl
+    @GetMapping("/formurl")
+    public String getForm(Model model){
+        model.addAttribute("key_form",new InnovaValidationDto());
+        return "formValidation";
+    }
+
+    // http://localhost:8090/formurl
+    @PostMapping("/formurl")
+    public String postForm(@ModelAttribute("key_form") InnovaValidationDto dto){
+        log.info(dto);
+        return "formValidation";
+    }
+}
