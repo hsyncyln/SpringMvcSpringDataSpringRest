c14f4afd6f074b89211fcb12651fc70e189d7fa2
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-12 16:51:37 +0300

    ResponseHeader & Cookies

--- src/main/java/com/innova/controller/HeaderBodyController.java
+++ src/main/java/com/innova/controller/HeaderBodyController.java
@@ -13,20 +13,76 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class HeaderBodyController {
 
+    // REQUEST HEADER
     //clientın Servera gizli olarak data göndermesi ->> token vs.
     //http://localhost:8090/controller/client/header
     @GetMapping("/controller/client/header")
     @ResponseBody
-    public  String getHeaderController1(){
+    public String getHeaderController() {
 
         String url = "http://localhost:8090/service/server/header";
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("key_header","Ben gizli bir bilgiyim");
+        httpHeaders.add("key_header", "Ben gizli bir bilgiyim");
 
         HttpEntity<String> httpEntity = new HttpEntity<String>("Bu data verisi", httpHeaders);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity,String.class);
-        return  response.toString();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+        return response.toString();
+    }
+
+    //RESPONSE HEADER
+    //Serverın Clienta gizli olarak data göndermesi ->> token vs.
+    //http://localhost:8090/controller/client/responseheader
+    @GetMapping("/controller/client/responseheader")
+    @ResponseBody
+    public String getResponseHeaderController() {
+
+        String url = "http://localhost:8090/service/server/responseheader";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> data = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY, String.class);
+        String gelenData = data.getHeaders().getFirst("key_response");
+        String body = data.getBody();
+        return "@Controller (Client) -> \n Body: " + body + "\n GelenData: " + gelenData;
+    }
+
+    //////////////////
+
+    //REQUEST COOKIE
+    //Serverın Clienta gizli olarak data göndermesi ->> token vs.
+    //http://localhost:8090/controller/client/cookie
+    @GetMapping("/controller/client/cookie")
+    @ResponseBody
+    public String getRequestCookieController() {
+
+        String url = "http://localhost:8090/service/server/cookie";
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.COOKIE, "key_cookie=cookieValue");
+
+        HttpEntity<String> httpEntity = new HttpEntity<String>("Bu data verisi", httpHeaders);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+        String result = response.getBody();
+        return "@Controller (Client): " + result;
+    }
+
+    //RESPONSE COOKIE
+    //Serverın Clienta gizli olarak cookie göndermesi
+    //http://localhost:8090/controller/client/responsecookie
+    @GetMapping("/controller/client/responsecookie")
+    @ResponseBody
+    public String getResponseCookieController() {
+
+        String url = "http://localhost:8090/service/server/responsecookie";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> data = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY, String.class);
+        String gelenData = data.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+        String body = data.getBody();
+
+        return "@Controller (Client) -> \n Body: " + body + "\n GelenData: " + gelenData;
     }
 }
--- src/main/java/com/innova/services/HeaderBodyServices.java
+++ src/main/java/com/innova/services/HeaderBodyServices.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpHeaders;
+
 //server
 @RestController
 @Log4j2
 public class HeaderBodyServices {
 
+    //REQUEST HEADER
+    //Amaç: Client header oluşturup server'a gönderir
     //http://localhost:8090/service/server/header
     @GetMapping("/service/server/header")
     public ResponseEntity<?> getHeader(
@@ -22,4 +26,43 @@ public class HeaderBodyServices {
         log.info(headerData);
         return  ResponseEntity.ok(headerData);
     }
+
+    //RESPONSE HEADER
+    //Amaç: Server header oluşturup client'a gönderir
+    //http://localhost:8090/service/server/responseheader
+    @GetMapping("/service/server/responseheader")
+    public ResponseEntity<?> getResponseHeader(){
+
+        return ResponseEntity.ok()
+                .header("key_response","Serverden gelen header response")
+                .body("@RestController: gelen veri");
+    }
+
+    //////////////
+
+    //REQUEST COOKIE
+    //Amaç: Clientın cookie oluşturup server'a göndermesi
+    //http://localhost:8090/service/server/cookie
+    @GetMapping("/service/server/cookie")
+    public ResponseEntity<?> getRequestCookie(
+            @RequestHeader(value="key_cookie", defaultValue = "default cookie") String gelenCookie
+    ){
+
+        String cookieData = gelenCookie;
+        log.info(cookieData);
+        return  ResponseEntity.ok(cookieData);
+    }
+
+    //RESPONSE COOKIE
+    //Amaç: Server cookie oluşturup client'a gönderir
+    //http://localhost:8090/service/server/responsecookie
+    @GetMapping("/service/server/responsecookie")
+    public ResponseEntity<?> getResponseCookie(){
+
+        return ResponseEntity.ok()
+                .header("key_cookie","Serverden gelen cookie response")
+                .body("@RestController: gelen veri");
+    }
+
+
 }
