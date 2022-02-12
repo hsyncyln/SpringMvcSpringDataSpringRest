c99d40562d933984a1fca6d4089983f0cc354d1f
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-12 18:41:43 +0300

    Servlet

--- src/main/java/com/innova/services/ServletExamples.java
+++ src/main/java/com/innova/services/ServletExamples.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @Log4j2
 @RestController
@@ -19,9 +22,22 @@ public class ServletExamples {
 
     // http://localhost:8090/servlet/info
     @GetMapping("/info")
+    @ResponseBody
     public ResponseEntity<?> getServletInformation(){
 
         String secretData = servletContext.getInitParameter("secretinformation");
         return ResponseEntity.ok(secretData);
+    }
+
+    // http://localhost:8090/servlet/common
+    @GetMapping("/common")
+    @ResponseBody
+    public ResponseEntity<?> getServletCommon(HttpServletRequest request){
+
+        log.info("info: " + request.getPathInfo());
+        log.info("method: " + request.getMethod());
+        log.info("port: " + request.getServerPort());
+
+        return ResponseEntity.ok(request.getServerPort());
     }
 }
