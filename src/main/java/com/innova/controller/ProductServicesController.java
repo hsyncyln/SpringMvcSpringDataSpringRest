829bb11e06c9c691c8f4b8cafae34d58438d0888
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-12 14:33:03 +0300

    Put Mapping

--- src/main/java/com/innova/controller/ProductServicesController.java
+++ src/main/java/com/innova/controller/ProductServicesController.java
@@ -5,13 +5,16 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
@@ -71,18 +74,84 @@ public class ProductServicesController {
     }
 
     //XML
-    // http://localhost:8090/client/controller/productdto/xml
-    @GetMapping("/client/controller/productdto/xml")
+    // http://localhost:8090/client/controller/xml
+    @GetMapping(value = "/client/controller/xml",produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
-    public List<ProductDto>  getXmlProductServicesResponseEntityList() {
-        String URL = "http://localhost:8090/rest/xml";
-        RestTemplate restTemplate = new RestTemplate();
-        //Anonymous Class
-        ResponseEntity<List<ProductDto>> responseEntity=restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<List<ProductDto>>() {});
-        List<ProductDto> list=responseEntity.getBody();
-        for (ProductDto temp:list){
-            log.info(temp);
-        }
-        return list;
+    public List<ProductDto> getXmlList(){
+
+        List<ProductDto> productDtoList = new ArrayList<>();
+        productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 1").productPrice(5000).build());
+        productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 2").productPrice(5000).build());
+        productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 3").productPrice(5000).build());
+
+        return productDtoList;
     }
+
+
+    ///////
+    //POST
+    // http://localhost:8090/client/controller/post
+    @GetMapping(value = "/client/controller/post")
+    @ResponseBody
+    public String  postBasic(){
+
+        ProductDto productDto = ProductDto.builder().productId(0L)
+                .productName("Urun").productPrice(1000).build();
+
+        String URL = "http://localhost:8090/post/basic";
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(URL,productDto,Void.class);
+
+        return "Success !";
+    }
+
+    // http://localhost:8090/client/controller/post/productDto?urun_adi=bilgisayar&urun_fiyati=6000
+    @ResponseBody
+    @GetMapping(value = "/client/controller/post/productDto")
+    public String  postProductDto(@RequestParam(name = "urun_adi") String urunAdi
+    ,@RequestParam(name = "urun_fiyati") double urunFiyati){
+
+        ProductDto productDto = ProductDto.builder().productId(0L)
+                .productName(urunAdi).productPrice(urunFiyati).build();
+
+        String URL = "http://localhost:8090/post/productDto";
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(URL,productDto,ProductDto.class);
+
+        return "Success !";
+    }
+
+    // http://localhost:8090/client/controller/post/responseentity?urun_adi=bilgisayar&urun_fiyati=6000
+    @ResponseBody
+    @GetMapping(value = "/client/controller/post/responseentity")
+    public String  postProductDtoResponseEntity(@RequestParam(name = "urun_adi") String urunAdi
+            ,@RequestParam(name = "urun_fiyati") double urunFiyati){
+
+        ProductDto productDto = ProductDto.builder().productId(0L)
+                .productName(urunAdi).productPrice(urunFiyati).build();
+
+        String URL = "http://localhost:8090/post/productDto";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<ProductDto> productDtoHttpEntity=new HttpEntity<>(productDto);
+        ResponseEntity<ProductDto> responseEntity=restTemplate.exchange(URL,HttpMethod.POST,productDtoHttpEntity,ProductDto.class);
+        ProductDto productDto2=responseEntity.getBody();
+        return "Success ! HttpEntity bir hata varsa almak icin: "+productDto2;
+    }
+
+    ///////////
+    //PUT
+    // http://localhost:8090/client/controller/put/responseentity?urun_adi=bilgisayar&urun_fiyati=6000
+    @GetMapping("/client/controller/put/responseentity")
+    @ResponseBody
+    public String  putProductDtoResponseEntity(@RequestParam(name="urun_adi") String urunAdi,@RequestParam(name="urun_fiyati") double urunFiyati) {
+        ProductDto productDto=ProductDto.builder().productId(0L).productName(urunAdi).productPrice(urunFiyati).build();
+        String URL = "http://localhost:8080/put/productdto";
+        RestTemplate restTemplate = new RestTemplate();
+        //HttpEntity aşağıda yazdım
+        ResponseEntity<ProductDto> responseEntity=restTemplate.exchange(URL,HttpMethod.PUT, new HttpEntity<ProductDto>(productDto),ProductDto.class);
+        ProductDto productDto2=responseEntity.getBody();
+        return "Update Success ! HttpEntity bir hata varsa almak icin: "+productDto2;
+    }
+
 }
--- src/main/java/com/innova/services/ProductService.java
+++ src/main/java/com/innova/services/ProductService.java
@@ -1,17 +1,19 @@
 package com.innova.services;
 
 import com.innova.dto.ProductDto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Log4j2
 @RestController
 public class ProductService {
 
     @GetMapping(value = "/rest/xml",produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
     public List<ProductDto> getXmlList(){
 
         List<ProductDto> productDtoList = new ArrayList<>();
@@ -20,5 +22,40 @@ public class ProductService {
         productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 3").productPrice(5000).build());
 
         return productDtoList;
+    }
+
+    @GetMapping(value = "/rest/json",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<ProductDto> getJsonList(){
+
+        List<ProductDto> productDtoList = new ArrayList<>();
+        productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 1").productPrice(5000).build());
+        productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 2").productPrice(5000).build());
+        productDtoList.add(ProductDto.builder().productId(0L).productName("Ürün 3").productPrice(5000).build());
+
+        return productDtoList;
+    }
+
+    ////
+    //POST
+
+    @PostMapping("post/basic")
+    public void postBasic(@RequestBody ProductDto dto){
+        log.info(dto);
+    }
+
+    @PostMapping("post/productDto")
+    public void postProduct(@RequestBody ProductDto dto){
+        log.info(dto);
+    }
+
+
+    ///////////////////////
+    //PUT
+    @PutMapping("put/productdto")
+    public ProductDto putProduct( @RequestBody ProductDto productDto){
+        log.info(productDto);
+        //database
+        return  productDto;
     }
 }
