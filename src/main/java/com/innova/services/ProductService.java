0ea661bfe41bad2aeef7f2a19e37f5af0ed8e561
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-02-12 15:36:19 +0300

    Status

--- src/main/java/com/innova/services/ProductService.java
+++ src/main/java/com/innova/services/ProductService.java
@@ -2,9 +2,12 @@ package com.innova.services;
 
 import com.innova.dto.ProductDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,5 +68,55 @@ public class ProductService {
     @DeleteMapping("delete/productdto/{id}")
     public void deleteProduct(@PathVariable("id") Long id){
         log.info("RestController -> Silinen dto id'si : " + id);
+    }
+
+    ///////////////////////
+    //STATUS CODE
+    //http://localhost:8090/rest/status1
+    @GetMapping("/rest/status1")
+    public ResponseEntity<ProductDto> getStatus1(){
+        ProductDto productDto = ProductDto.builder()
+                .productId(0L).productName("Telefon 11")
+                .productPrice(6000).build();
+
+        //Hatasız çalışıyorsa ==> OK
+        //return new ResponseEntity<>(productDto, HttpStatus.OK);
+        //return ResponseEntity.status(HttpStatus.OK).body(productDto);
+        return ResponseEntity.ok().body(productDto);
+    }
+    //http://localhost:8090/rest/notfound/status2/0
+    @GetMapping("/rest/notfound/status2/{id}")
+    public ResponseEntity<?> getStatus2(@PathVariable(name = "id") Long id){
+        ProductDto productDto = ProductDto.builder().productId(id)
+                .productName("Telefon 11").productPrice(6000).build();
+
+        if(productDto.getProductId() == 0) {
+            // return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Kayıt bulunamadı..");
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("Kayıt bulundu..." + productDto);
+    }
+
+    //http://localhost:8090/rest/badrequest/status3/0
+    @GetMapping("/rest/badrequest/status3/{id}")
+    public ResponseEntity<?> getStatus3(@PathVariable(name = "id") Long id){
+        ProductDto productDto = ProductDto.builder().productId(id)
+                .productName("Telefon 11").productPrice(6000).build();
+
+        if(productDto.getProductId() == 0) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("Kayıt bulundu..." + productDto);
+    }
+
+    //http://localhost:8090/rest/notfound/ok4/0
+    @GetMapping("/rest/notfound/ok4/{id}")
+    public ResponseEntity<?> getStatus4(@PathVariable(name = "id") Long id){
+        ProductDto productDto = ProductDto.builder().productId(id)
+                .productName("Telefon 2").productPrice(7000).build();
+
+        return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.OK)
+                .contentType(new MediaType("application","json"
+                        , Charset.forName("UTF-8"))).body("Tamamdır.");
     }
 }
